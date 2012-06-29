430b1cedca7984f48b9f2b940ae961b37eb40303
Bryce Nordgren <bnordgren@gmail.com>
2012-06-29 18:02:08 +0200

    Added for database config.

--- modules/services/web-servers/geonetwork/builder.sh
+++ modules/services/web-servers/geonetwork/builder.sh
@@ -3,4 +3,14 @@ source $stdenv/setup
 cd $TMPDIR
 $jdk/bin/jar xf $geonetwork/$warfile
 
-cp -r . $out
+cat > db << END 
+${databaseConfig}
+END
+
+sed -e "s#%UPLOAD%#${uploadDir}#g" \
+    -e "/%DATABASE_CONFIG%/r db" \
+    -e "/%DATABASE_CONFIG%/d" $configTemplate > WEB-INF/config.xml
+
+mkdir -p $out
+$jdk/bin/jar cf $out/geonetwork.war .
+
--- modules/services/web-servers/geonetwork/default.nix
+++ modules/services/web-servers/geonetwork/default.nix
@@ -5,14 +5,17 @@ with pkgs.lib;
 let
 
   cfg = config.services.geonetwork;
+  tomcatCfg = config.services.tomcat;
   tomcat = pkgs.tomcat6;
   geonetwork = pkgs.geonetwork;
 
   geonetworkconf = pkgs.stdenv.mkDerivation { 
     name    = "geonetwork-conf" ;
     builder = ./builder.sh ;
+    configTemplate = ./config-template.xml;
     inherit (pkgs) geonetwork jdk ; 
     inherit (pkgs.geonetwork) warfile ; 
+    inherit (cfg) uploadDir databaseConfig ; 
   } ; 
 in
 
@@ -31,6 +34,121 @@ in
         description = "Whether to enable Geonetwork Opensource";
       };
 
+      uploadDir = mkOption { 
+        default = "/var/geonetwork" ; 
+        description = "Directory to contain files uploaded to Geonetwork" ; 
+      };
+
+      databaseConfig = mkOption { 
+        description = "Geonetwork XML configuration for database connection" ; 
+        default = ''		<resource enabled="true">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>BayACrsQ</user>
+				<password>Qf7Po9T0</password>
+				<driver>com.mckoi.JDBCDriver</driver>
+				<url>jdbc:mckoi://localhost:9157/</url>
+				<poolSize>10</poolSize>
+			</config>
+
+
+		<activator class="org.fao.geonet.activators.McKoiActivator"><configFile>WEB-INF/db/db.conf</configFile></activator>
+                </resource>
+'';
+
+        example = ''
+Choose only one of the following resources and ensure that "enabled" = true
+		<resource enabled="true">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>BayACrsQ</user>
+				<password>Qf7Po9T0</password>
+				<driver>com.mckoi.JDBCDriver</driver>
+				<url>jdbc:mckoi://localhost:9157/</url>
+				<poolSize>10</poolSize>
+			</config>
+
+
+		<activator class="org.fao.geonet.activators.McKoiActivator"><configFile>WEB-INF/db/db.conf</configFile></activator>
+                </resource>
+
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		<!-- mysql -->
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<resource enabled="false">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>admin</user>
+				<password>admin</password>
+				<driver>com.mysql.jdbc.Driver</driver>
+				<url>jdbc:mysql://$WEBSERVER_HOST/geonetwork</url>
+				<poolSize>10</poolSize>
+				<reconnectTime>3600</reconnectTime>
+			</config>
+		</resource>
+
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		<!-- oracle -->
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<resource enabled="false">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>admin</user>
+				<password>admin</password>
+				<driver>oracle.jdbc.driver.OracleDriver</driver>
+				<url>jdbc:oracle:thin:@IP:1521:fs</url>
+				<poolSize>10</poolSize>
+			</config>
+		</resource>
+	    
+	    
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		<!-- postgresql -->
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		
+		<resource enabled="false">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>www-data</user>
+				<password>www-data</password>
+				<driver>org.postgresql.Driver</driver>
+				<!--					
+					jdbc:postgresql:database
+					jdbc:postgresql://host/database
+					jdbc:postgresql://host:port/database
+				-->
+                <url>jdbc:postgresql:geonetwork</url>
+				<poolSize>10</poolSize>
+			</config>
+		</resource>
+	    
+	    
+	    <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+		<!-- sqlserver 2008 -->
+		<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<resource enabled="false">
+			<name>main-db</name>
+			<provider>jeeves.resources.dbms.DbmsPool</provider>
+			<config>
+				<user>www-data</user>
+				<password>www-data</password>
+				  <driver>com.microsoft.sqlserver.jdbc.SQLServerDriver</driver>
+                  <url>jdbc:sqlserver://SERVER;database=geonetwork;integratedSecurity=false;</url>
+				<poolSize>10</poolSize>
+			</config>
+		</resource>
+'' ; 
+ 
+      } ; 
+
     };
 
   };
@@ -42,9 +160,20 @@ in
 
     services.tomcat = {
        enable = config.services.geonetwork.enable ; 
-       webapps = [ geonetworkconf ] ; 
+       webapps = [ "${geonetworkconf}/geonetwork.war" ] ; 
     } ;
 
+    jobs.geonetwork = { 
+       description = "Geonetwork server (deployed in Tomcat)" ; 
+       startOn = "started tomcat" ; 
+       stopOn  = "stopping tomcat" ; 
+
+       preStart = ''
+            # create the upload directory
+            mkdir -p ${cfg.uploadDir}
+            chown -R ${tomcatCfg.user}.${tomcatCfg.group} ${cfg.uploadDir}
+       '' ;
+    } ; 
   };
 
 }
